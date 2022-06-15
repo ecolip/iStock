40ea20a60f90ecc642c99121abc77ecb773b3614
Emma Cheng <ecolipp@gmail.com>
2022-06-15 15:54:57 +0800

    Add api: finMindLogin, getHistoryPrice, getTodayPrice.

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,27 @@
 const api = {
   hostname: 'https://api.finmindtrade.com/api/v4/data?',
-  token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRlIjoiMjAyMi0wNi0xMyAyMToxMjowMCIsInVzZXJfaWQiOiJlY29saXAiLCJpcCI6IjExOC4xNjguNDEuMTg2In0.SlGERKyj3V45rngX5UqHGLvIoz-1PRGStCbHLvMvqlM',
-  getDateInfo() {
-    return fetch(`${this.hostname}dataset=TaiwanStockDividend`, {
+  startDate: '2012-05-14',
+  today: '2022-06-14',
+  finMindLogin() {
+    const formData = new FormData();
+    formData.append('user_id', process.env.REACT_APP_FINMIND_USER_ID);
+    formData.append('password', process.env.REACT_APP_FINMIND_PASSWORD);
+    const payload = new URLSearchParams(formData);
+    return fetch('https://api.finmindtrade.com/api/v4/login', {
+      body: payload,
       headers: new Headers({
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${this.token}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
       }),
+      method: 'POST',
     }).then((response) => response.json());
+  },
+  getTodayPrice(token, id) {
+    return fetch(`${this.hostname}token=${token}&data_id=${id}&start_date=${this.today}&end_date=${this.today}&dataset=TaiwanStockPrice`)
+      .then((response) => response.json());
+  },
+  getHistoryPrice(token, id) {
+    return fetch(`${this.hostname}token=${token}&dataset=TaiwanStockPrice&data_id=${id}&start_date=${this.startDate}&end_date=${this.today}`)
+      .then((response) => response.json());
   },
 };
 
