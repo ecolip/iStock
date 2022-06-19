cc4506737885612e5fc5361c871fe2d702ea7067
Emma Cheng <ecolipp@gmail.com>
2022-06-19 23:20:43 +0800

    Adjust pre year date for track candlestick.

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -11,7 +11,7 @@ const today = () => {
 
 const preYear = () => {
   const date = new Date();
-  const year = date.getFullYear() - 1;
+  const year = date.getFullYear() - 7;
   const month = date.getMonth() + 1;
   const day = date.getDate();
   if (month < 10) {
@@ -20,4 +20,23 @@ const preYear = () => {
   return `${year}-${month}-${day}`;
 };
 
-export { today, preYear };
+const specifyDay = (dateObj) => {
+  const year = dateObj.getFullYear();
+  const month = dateObj.getMonth() + 1;
+  const day = dateObj.getDate();
+  if (month < 10) {
+    return `${year}-0${month}-${day}`;
+  }
+  return `${year}-${month}-${day}`;
+};
+
+const preDay = (date) => {
+  const dateObj = new Date(date);
+  const time = dateObj.getTime();
+  const oneDay = 24 * 60 * 60 * 1000;
+  const preTime = time - oneDay;
+  dateObj.setTime(preTime);
+  return specifyDay(dateObj);
+};
+
+export { today, preYear, preDay };
