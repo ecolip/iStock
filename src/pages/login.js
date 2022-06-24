837c303e5d97ed7b5912ddef167be7a955379613
Emma Cheng <56777887+ecolip@users.noreply.github.com>
2022-06-24 11:49:32 +0800

    Improve CSS: login page.

--- src/pages/login.js
+++ src/pages/login.js
@@ -28,7 +28,7 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  /* height: 100vh; */
+  min-height: 100vh;
   margin: 0 auto;
   background-color: #181A20;
 `;
